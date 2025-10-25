28e18d447de3676f710be4712cf8da1d49631fe3
FahadAliHashmi2011 <faiezali2006@gmail>
2025-10-25 17:10:19 +0100

    xcd

--- index.py
+++ index.py
@@ -44,3 +44,16 @@ try:
     calculate_change(total, paid)
 except ValueError:
     print("Please enter valid numbers.")
+
+
+
+
+
+import math  
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
+
+print(math.ceil(45.490))
+print(math.floor(45.890))
+print(math.factorial(5))
